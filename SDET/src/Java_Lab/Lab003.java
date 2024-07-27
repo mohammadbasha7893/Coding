2800e89c9e35d7b8873f4521c36c46d55362e64b
Mahammadbasha <mohammadbasha7893@gmail.com>
2024-07-27 12:07:17 +0530

    Abich bejiu rey

--- SDET/src/Java_Lab/Lab003.java
+++ SDET/src/Java_Lab/Lab003.java
@@ -1,2 +1,13 @@
-package Java_Lab;public class Lab003 {
+package Java_Lab;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        String s="mahammad basha";
+        int nm=12;
+        float f=48943f;
+        double d=783.6;
+        boolean b=true;
+        System.out.println(nm);
+
+    }
 }
---
+++ SDET/src/Java_Lab/Lab004.java
@@ -0,0 +1,11 @@
+package Java_Lab;
+
+public class Lab004 {
+    public static void main(String[] args) {
+        //operators
+        int a=10;
+        int b=30;
+        int c=a+b;
+        System.out.println(c);
+    }
+}
